3ec17be38a7a8d80c9b42705e314f2272970aa59
Matsuyanagi <1475814+Matsuyanagi@users.noreply.github.com>
2023-11-19 02:16:40 +0900

    QNumber<T> 定義
    IBinaryInteger<T> を継承して数値型としてふるまう。
    QNumberBigInteger QNumber<BigInteger> として BigInteger を内部実装として利用する。

--- ecc_20231118_curve448_toy/Program.cs
+++ ecc_20231118_curve448_toy/Program.cs
@@ -4,7 +4,8 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			QNumberBigInteger qb = new (1);
+			Console.WriteLine(qb.ToString());
 		}
 	}
 }
---
+++ ecc_20231118_curve448_toy/QNumberBigInteger.cs
@@ -0,0 +1,73 @@
+﻿using System.Numerics;
+
+namespace ecc_20231118_curve448_toy
+{
+	internal class QNumberBigInteger : QNumber<BigInteger>
+	{
+
+		public QNumberBigInteger(Int32 x)
+		{
+			innerValue = x;
+		}
+
+		public QNumberBigInteger(UInt32 x)
+		{
+			innerValue = x;
+		}
+
+		public QNumberBigInteger(Int64 x)
+		{
+			innerValue = x;
+		}
+
+		public QNumberBigInteger(UInt64 x)
+		{
+			innerValue = x;
+		}
+
+		public QNumberBigInteger(Int128 x)
+		{
+			innerValue = x;
+		}
+
+		public QNumberBigInteger(UInt128 x)
+		{
+			innerValue = x;
+		}
+
+		public QNumberBigInteger(BigInteger x)
+		{
+			innerValue = x;
+		}
+
+		public QNumberBigInteger(Decimal x)
+		{
+			innerValue = (BigInteger)x;
+		}
+
+		public QNumberBigInteger(Single x)
+		{
+			innerValue = (BigInteger)x;
+		}
+
+		public QNumberBigInteger(Double x)
+		{
+			innerValue = (BigInteger)x;
+		}
+
+		public string? ToString(string? format)
+		{
+			return RawValue.ToString(format);
+		}
+
+		public string? ToString(IFormatProvider? provider)
+		{
+			return RawValue.ToString(provider);
+		}
+
+		public string? ToString(string? format, IFormatProvider? provider)
+		{
+			return RawValue.ToString(format, provider);
+		}
+	}
+}
