e2f110e1456363bacfc7409599de90c246a81393
Matsuyanagi <1475814+Matsuyanagi@users.noreply.github.com>
2023-12-06 00:10:37 +0900

    拡張同次座標 EHPoint 実装

---
+++ Tests/EHPointTest.cs
@@ -0,0 +1,16 @@
+﻿using ecc_20231118_curve448_toy;
+
+namespace Tests
+{
+	internal class EHPointTest
+	{
+		[TestCase(0, 1, 1, "(0,1,1)")]
+		[TestCase(0, 2, 2, "(0,2,2)")]
+		[TestCase(-1, -100, -10, "(-1,-100,-10)")]
+		public void TestLessThan(Int64 x, Int64 y, Int64 z, string s)
+		{
+			Assert.That((new EHPoint(x, y, z)).ToString(), Is.EqualTo(s));
+		}
+
+	}
+}
---
+++ ecc_20231118_curve448_toy/EHPoint.cs
@@ -0,0 +1,30 @@
+﻿namespace ecc_20231118_curve448_toy
+{
+	/// <summary>
+	/// Extended Homogeneous Coordinates Point
+	/// 拡張同次座標
+	/// </summary>
+	public struct EHPoint
+	{
+		public QNumberBigInteger X { get; }
+		public QNumberBigInteger Y { get; }
+		public QNumberBigInteger Z { get; }
+
+		public EHPoint() : this(0, 0, 1)
+		{
+		}
+
+		public EHPoint(Int64 x, Int64 y, Int64 z = 1)
+		{
+			X = new QNumberBigInteger(x);
+			Y = new QNumberBigInteger(y);
+			Z = new QNumberBigInteger(z);
+		}
+		public static EHPoint Identity => new EHPoint(0, 1, 1);
+
+		public override string ToString()
+		{
+			return $"({X},{Y},{Z})";
+		}
+	}
+}
