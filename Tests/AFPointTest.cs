4496f41b667ae240f93e4c7f5deb82d38ae61440
Matsuyanagi <1475814+Matsuyanagi@users.noreply.github.com>
2023-12-10 16:25:07 +0900

    AFPoint 追加

---
+++ Tests/AFPointTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ecc_20231118_curve448_toy;
+
+namespace Tests
+{
+	internal class AFPointTest
+	{
+		[TestCase(0, 1, "(0,1)")]
+		[TestCase(0, 2, "(0,2)")]
+		[TestCase(-1, -100, "(-1,-100)")]
+		public void TestEqualString(Int64 x, Int64 y, string s)
+		{
+			Assert.That((new AFPoint(x, y)).ToString(), Is.EqualTo(s));
+		}
+
+	}
+}
---
+++ ecc_20231118_curve448_toy/AFPoint.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ecc_20231118_curve448_toy
+{
+	/// <summary>
+	/// Affine Point
+	/// アフィン座標点
+	/// </summary>
+	public readonly struct AFPoint(Int64 x, Int64 y) : IEquatable<AFPoint>
+	{
+		public QNumberBigInteger X { get; } = new QNumberBigInteger(x);
+		public QNumberBigInteger Y { get; } = new QNumberBigInteger(y);
+
+		public AFPoint() : this(0, 0)
+		{
+		}
+
+		public static AFPoint Identity => new(0, 1);
+
+		public override readonly string ToString() => $"({X},{Y})";
+
+		public readonly bool Equals(AFPoint other) => X == other.X && Y == other.Y;
+		public override bool Equals(object? obj) => obj is AFPoint objS && Equals(objS);
+
+		public override int GetHashCode() => X.GetHashCode() + Y.GetHashCode();
+	}
+}
