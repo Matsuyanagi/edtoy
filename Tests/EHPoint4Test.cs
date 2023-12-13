fb86dc45332bf3c540b86d08906caec8b9cae86b
Matsuyanagi <1475814+Matsuyanagi@users.noreply.github.com>
2023-12-13 23:58:34 +0900

    EHPoint4 追加

---
+++ Tests/EHPoint4Test.cs
@@ -0,0 +1,30 @@
+﻿using ecc_20231118_curve448_toy;
+
+namespace Tests
+{
+	internal class EHPoint4Test
+	{
+		[TestCase(0, 1, 1, 0, "(0,1,1,0)")]
+		[TestCase(0, 2, 2, 0, "(0,2,2,0)")]
+		[TestCase(-1, -100, -10, 100, "(-1,-100,-10,100)")]
+		public void TestEqualString(Int64 x, Int64 y, Int64 z, Int64 prime, string s)
+		{
+			Assert.That(new EHPoint4(x, y, z, prime).ToString(), Is.EqualTo(s));
+		}
+
+		[TestCase(0, 1, 1, 17, 0, 1)]
+		[TestCase(0, 10, 5, 17, 0, 2)]
+		[TestCase(100, 10, 8, 19, 3, 6)]
+		public void TestToAFPoint(Int64 x, Int64 y, Int64 z, Int64 prime, Int64 ax, Int64 ay)
+		{
+			Assert.That(new EHPoint4(x, y, z, prime).ToAFPoint(prime), Is.EqualTo(new AFPoint(ax, ay)));
+		}
+
+		[TestCase(100, 10, 47, 100, 10, 1)]
+		[TestCase(-10, -1, 47, -10, -1, 1)]
+		public void TestFromAFPoint(Int64 ax, Int64 ay, Int64 prime, Int64 x, Int64 y, Int64 z)
+		{
+			Assert.That(new EHPoint4(new AFPoint(ax, ay), prime), Is.EqualTo(new EHPoint4(x, y, 1, prime)));
+		}
+	}
+}
---
+++ ecc_20231118_curve448_toy/EHPoint4.cs
@@ -0,0 +1,63 @@
+﻿namespace ecc_20231118_curve448_toy
+{
+	public readonly struct EHPoint4(QNumberBigInteger x, QNumberBigInteger y, QNumberBigInteger z, QNumberBigInteger prime) : IEquatable<EHPoint4>
+	{
+		public QNumberBigInteger X { get; } = x;
+		public QNumberBigInteger Y { get; } = y;
+		public QNumberBigInteger Z { get; } = z;
+		public QNumberBigInteger T { get; } = x.MulMod(y, prime);
+
+		public QNumberBigInteger Prime { get; } = prime;
+
+		public EHPoint4() : this(0, 0, 1, 0)
+		{
+		}
+
+		public EHPoint4(AFPoint ap, QNumberBigInteger prime) : this(ap.X, ap.Y, 1, ap.X.MulMod(ap.Y, prime))
+		{
+		}
+
+		private static readonly EHPoint4 _identity = new(0, 1, 1, 0);
+		public static EHPoint4 Identity => _identity;
+
+		public override string ToString() => $"({X},{Y},{Z},{T})";
+
+		public bool Equals(EHPoint4 other) => X == other.X && Y == other.Y && Z == other.Z && T == other.T;
+
+		public override bool Equals(object? obj) => obj is EHPoint4 objS && Equals(objS);
+
+		public override int GetHashCode() => X.GetHashCode() + Y.GetHashCode() + Z.GetHashCode() + Y.GetHashCode();
+
+		public AFPoint ToAFPoint(QNumberBigInteger prime) => new(X.DivMod(Z, prime), Y.DivMod(Z, prime));
+
+
+		public static EHPoint4 EdwardsCurveAdd(EHPoint4 p1, EHPoint4 p2, QNumberBigInteger a, QNumberBigInteger d)
+		{
+			// https://hyperelliptic.org/EFD/g1p/auto-twisted-extended.html
+			var prime = p1.Prime;
+			var A = p1.X.MulMod(p2.X, prime);
+			var B = p1.Y.MulMod(p2.Y, prime);
+			var C = p1.Z.MulMod(p2.T, prime);
+			var D = p1.T.MulMod(p2.Z, prime);
+			var E = D.AddMod(C, prime);
+			var F = p1.X.AddMod(-p1.Y, prime).MulMod(p2.X.AddMod(p2.Y, prime).AddMod(B, prime).AddMod(-A, prime), prime);
+			var G = B.AddMod(a.MulMod(A, prime), prime);
+			var H = D.AddMod(-C, prime);
+
+			var px = E.MulMod(F, prime);
+			var py = G.MulMod(H, prime);
+			var pz = F.MulMod(G, prime);
+			return new EHPoint4(px, py, pz, prime);
+		}
+
+		public static bool operator ==(EHPoint4 left, EHPoint4 right)
+		{
+			return left.Equals(right);
+		}
+
+		public static bool operator !=(EHPoint4 left, EHPoint4 right)
+		{
+			return !(left == right);
+		}
+	}
+}
