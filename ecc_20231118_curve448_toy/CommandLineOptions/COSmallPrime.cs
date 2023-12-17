cd80226e12d9e4b680368c4805704ba8c252e1b3
Matsuyanagi <1475814+Matsuyanagi@users.noreply.github.com>
2023-12-17 23:38:19 +0900

    CommandLineOptions COPrime と COSmallPrime クラス作成

---
+++ ecc_20231118_curve448_toy/CommandLineOptions/COSmallPrime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace ecc_20231118_curve448_toy.CommandLineOptions
+{
+	[Verb("smallprime", HelpText = "Small prime number list (<65536)")]
+	internal class COSmallPrime
+	{
+	}
+}
--- ecc_20231118_curve448_toy/Program.cs
+++ ecc_20231118_curve448_toy/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace ecc_20231118_curve448_toy
+﻿using CommandLine;
+using ecc_20231118_curve448_toy.CommandLineOptions;
+
+namespace ecc_20231118_curve448_toy
 {
 	internal static class Program
 	{
 		static void Main(string[] args)
 		{
+			Parser.Default.ParseArguments<COPrime, COSmallPrime>(args)
+				.WithParsed<COPrime>(opt => { })
+				.WithParsed<COSmallPrime>(opt => { })
+				.WithNotParsed(err => { });
 		}
 	}
 }
