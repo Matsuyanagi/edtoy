6537892670e5918060a2b8f1501504274624c616
Matsuyanagi <1475814+Matsuyanagi@users.noreply.github.com>
2024-01-04 02:45:16 +0900

    fix curvepoint_add で -l length オプションの値が反映されていなかったのを修正。

--- ecc_20231118_curve448_toy/SubCommands/CurvePointAddListCommand.cs
+++ ecc_20231118_curve448_toy/SubCommands/CurvePointAddListCommand.cs
@@ -9,7 +9,7 @@ using ecc_20231118_curve448_toy.EdwardsCurveComponents;
 
 namespace ecc_20231118_curve448_toy.SubCommands
 {
-    public static class CurvePointAddListCommand
+	public static class CurvePointAddListCommand
 	{
 		public static void Run(COCurvePointAddList option)
 		{
@@ -18,19 +18,19 @@ namespace ecc_20231118_curve448_toy.SubCommands
 			QNumberBigInteger param_d = option.ParamD;
 			int number = Util.BalanceNumberLimit(option.Number, prime);
 
-			foreach (var base_point in CurvePointList.EdwardsCurvePointList(prime, param_a, param_d,option.Random).Take(number))
+			foreach (var base_point in CurvePointList.EdwardsCurvePointList(prime, param_a, param_d, option.Random).Take(number))
 			{
 				Console.Write($"({base_point.X},{base_point.Y}):");
 				AFPoint point = base_point;
 
 				var n = 0;
-				while (point != AFPoint.Identity && n < number)
+				while (point != AFPoint.Identity && n < option.Length)
 				{
 					point = AFPoint.EdwardsCurveAdd(point, base_point, param_a, param_d, prime);
 					Console.Write($"({point.X},{point.Y})");
 					n += 1;
 				}
-				if (n == number)
+				if (n == option.Length)
 				{
 					Console.WriteLine("...");
 				}
